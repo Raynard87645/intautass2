ecaa4fdb16cddc65e55038517c4f1744773cb3b6
Raynard Henry <henryraynard90@gmail.com>
2025-03-07 10:11:20 -0500

    Initial commit

---
+++ layout/navbar.php
@@ -0,0 +1,64 @@
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+  <div class="container-fluid">
+    <a class="navbar-brand" href="#">Navbar</a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+        <li class="nav-item">
+          <a class="nav-link active" aria-current="page" href="/views/home.php">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/views/welcome.php">Welcome</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/views/product.php">Products</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/views/contact.php">Contact</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/views/auth/login.php">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="/views/register/login.php">Register</a>
+        </li>
+        <!-- <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Assignment
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="/assignments/assignment1.php">Asignment 1</a></li>
+            <li><a class="dropdown-item" href="#">Assignment 2</a></li>
+            <li><a class="dropdown-item" href="#">Assignment 3</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Activity
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Asignment 1</a></li>
+            <li><a class="dropdown-item" href="#">Assignment 2</a></li>
+            <li><a class="dropdown-item" href="#">Assignment 3</a></li>
+          </ul>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Class Activity
+          </a>
+          <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+            <li><a class="dropdown-item" href="#">Asignment 1</a></li>
+            <li><a class="dropdown-item" href="#">Assignment 2</a></li>
+            <li><a class="dropdown-item" href="#">Assignment 3</a></li>
+          </ul>
+        </li> -->
+      </ul>
+      <form class="d-flex">
+        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <button class="btn btn-outline-success" type="submit">Search</button>
+      </form>
+    </div>
+  </div>
+</nav>
